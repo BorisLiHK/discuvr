557f9c93022c5765493aefc2cbfd30b9cd93d945
Alex Dacre <alex@alexdacre.com>
2016-09-05 08:52:13 +1000

    Still working at getting a layer

--- imports/ui/MapContainer.jsx
+++ imports/ui/MapContainer.jsx
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 
 import Map from './Map';
+import Layer from './Layer'
+import Feature from "./Feature";
 import AccountsUIWrapper from './AccountsUIWrapper'
 
 export default class MapContainer extends Component {
@@ -22,11 +24,30 @@ export default class MapContainer extends Component {
         return [151.2093, -33.8688]
     }
 
+    _onToggleHover(cursor, { map }) {
+        map.getCanvas().style.cursor = cursor;
+    }
+
     render() {
         return (
             <div className="super_class">
                 <Map
-                    center={this.state.mapCenter} />
+                    center={this.state.mapCenter}>
+                    <Layer
+                        type="symbol"
+                        id="marker"
+                        layout={{ "icon-image": "marker-15" }}>
+
+                        <Feature
+                            key={0}
+                            onHover={this._onToggleHover.bind(this, "pointer")}
+                            onEndHover={this._onToggleHover.bind(this, "")}
+                            coordinates={[
+                                151.1470661,
+                                -33.9092054
+                            ]}/>
+                    </Layer>
+                </Map>
                 <AccountsUIWrapper />
             </div>
         );
