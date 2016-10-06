8f1a7a3091613c1bc96f631fbe817884b72ad750
Hayden4050 <hayden.fowler@gmail.com>
2016-10-06 14:45:44 +1100

    Added control to select members

--- imports/ui/CreateCirclePage.jsx
+++ imports/ui/CreateCirclePage.jsx
@@ -1,15 +1,38 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
+import { createContainer } from 'meteor/react-meteor-data';
 import {Form} from 'simple-react-form';
 import RaisedButton from 'material-ui/RaisedButton';
 import {browserHistory} from 'react-router';
+import FilteredMultiSelect from 'react-filtered-multiselect';
 
 import Circles from '../api/circles';
 
-export default class CreateCirclePage extends Component {
-    handleSubmit(event) {
-        Console.log(this);
+class CreateCirclePage extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            CULTURE_SHIPS: this.props.CULTURE_SHIPS,
+            selectedShips: this.props.selectedShips
+        }
+
+        this.addShip = this.addShip.bind(this)
+        this.removeShip = this.removeShip.bind(this)
     }
+
+    addShip(newSelectedShips) {
+        this.setState({selectedShips: newSelectedShips}) 
+        console.log(newSelectedShips)
+        console.log(this.state.selectedShips)
+    }
+
+    removeShip(index) {
+        var newSelectedShips = this.state.selectedShips.slice()
+        newSelectedShips.splice(index, 1)
+        this.setState({selectedShips: newSelectedShips})
+    }
+
     render() {
         return (
             <div>
@@ -23,6 +46,40 @@ export default class CreateCirclePage extends Component {
                     }
                     logErrors
                 />
+
+                <div style={{marginTop: 20}}>
+                    <FilteredMultiSelect
+                        buttonText="Add Ship"
+                        onChange={this.addShip}
+                        options={this.state.CULTURE_SHIPS}
+                        selectedOptions={this.state.selectedShips}
+                        textProp="name"
+                        valueProp="id"
+                        classNames={{
+                            button: 'FilteredMultiSelect__button',
+                            // Used when at least one <option> is selected
+                            buttonActive: 'FilteredMultiSelect__button--active',
+                            filter: 'FilteredMultiSelect__filter',
+                            select: 'FilteredMultiSelect__select'
+                        }}
+                    />
+                    <FilteredMultiSelect
+                        buttonText="Remove Ship"
+                        onChange={this.removeShip}
+                        options={this.state.selectedShips}
+                        textProp="name"
+                        valueProp="id"
+                        classNames={{
+                            button: 'FilteredMultiSelect__button',
+                            // Used when at least one <option> is selected
+                            buttonActive: 'FilteredMultiSelect__button--active',
+                            filter: 'FilteredMultiSelect__filter',
+                            select: 'FilteredMultiSelect__select'
+                        }}
+                        style={{marginTop: 20}}
+                    />
+                </div>
+
                 <div style={{marginTop: 20}}>
                     <RaisedButton label='Create' primary={true} onTouchTap={() => this.refs.form.submit()} style={{
                         position: 'fixed',
@@ -47,3 +104,24 @@ export default class CreateCirclePage extends Component {
         );
     }
 }
+
+CreateCirclePage.PropTypes = {
+    members: PropTypes.array,
+    selectedMembers: PropTypes.array,
+    CULTURE_SHIPS: PropTypes.array, 
+    selectedShips: PropTypes.array,
+}
+
+export default createContainer(() => {
+    Meteor.subscribe('userList');
+
+    return {
+        CULTURE_SHIPS: [
+            {id: 1, name: '5*Gelish-Oplule'},
+            {id: 2, name: '7*Uagren'},
+            {id: 249, name: 'Zero Gravitas'},
+            {id: 250, name: 'Zoologist'}
+        ],
+        selectedShips: []
+    };
+}, CreateCirclePage)
